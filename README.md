6e7e71dc9e57e8eea5c5a0881322c8d2bd0d4101
FERAS ABDALRAHMAN <doonfrs@gmail.com>
2023-04-08 21:24:26 +0300

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,92 @@
-# hover_menu
+## Hover Menu for Flutter
+A Flutter package for creating a hover menu for desktop and web applications, providing a common web menu experience.
+
+### Features
+- Easy-to-use hover menu that appears when the user hovers over a specified title widget
+- Closes the menu when the user stops hovering over the menu or its items
+- Customizable
+
+### Installation
+To use this package, add hover_menu as a dependency in your pubspec.yaml file.
+
+    dependencies:
+      hover_menu: ^1.0.0
+
+Then, run flutter packages get in your terminal.
+
+### Usage
+Import the package into your Dart file:
+
+    import 'package:hover_menu/hover_menu.dart';
+
+Create a HoverMenu widget in your app:
+
+    HoverMenu(
+      title: Text('Menu Title'),
+      items: [
+        ListTile(title: Text('Item 1')),
+        ListTile(title: Text('Item 2')),
+        ListTile(title: Text('Item 3')),
+      ],
+    )
+    
+Optionally, you can set a custom width for the menu:
+
+    HoverMenu(
+      title: Text('Menu Title'),
+      items: [
+        ListTile(title: Text('Item 1')),
+        ListTile(title: Text('Item 2')),
+        ListTile(title: Text('Item 3')),
+      ],
+      width: 250,
+    )
+
+## Example
+Here is an example of how to use the HoverMenu widget in your application:
+    import 'package:flutter/material.dart';
+    import 'package:hover_menu/hover_menu.dart';
+    
+    void main() {
+      runApp(MyApp());
+    }
+    
+    class MyApp extends StatelessWidget {
+      @override
+      Widget build(BuildContext context) {
+        return MaterialApp(
+          home: Scaffold(
+            appBar: AppBar(title: Text('Hover Menu Example')),
+            body: Center(
+              child: HoverMenu(
+                title: InkWell(
+                  onTap: () {},
+                  child: Text('Menu Title'),
+                ),
+                items: [
+                  InkWell(
+                    onTap: () {
+                      print('Item 1 clicked');
+                    },
+                    child: ListTile(title: Text('Item 1')),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print('Item 2 clicked');
+                    },
+                    child: ListTile(title: Text('Item 2')),
+                  ),
+                  InkWell(
+                    onTap: () {
+                      print('Item 3 clicked');
+                    },
+                    child: ListTile(title: Text('Item 3')),
+                  ),
+                ],
+              ),
+            ),
+          ),
+        );
+      }
+    }
+    
---
+++ example/lib/main.dart
@@ -0,0 +1,106 @@
+import 'package:flutter/material.dart';
+import 'package:hover_menu/hover_menu.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Hover Menu',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'Hover Menu'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        centerTitle: true,
+        title: Text(widget.title),
+      ),
+      body: Column(
+        children: [
+          Container(
+            width: double.infinity,
+            decoration: const BoxDecoration(
+                border:
+                    Border(bottom: BorderSide(color: Colors.black, width: 1))),
+            height: 50,
+            child: Row(
+              mainAxisSize: MainAxisSize.min,
+              children: [
+                HoverMenu(
+                  title:
+                      const Text('Main', style: TextStyle(color: Colors.black)),
+                  items: const [
+                    ListTile(
+                      mouseCursor: SystemMouseCursors.click,
+                      title: Text(
+                        'Sub',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    ListTile(
+                      mouseCursor: SystemMouseCursors.click,
+                      title: Text(
+                        'Sub 2',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+                HoverMenu(
+                  title: const Text('Menu 2',
+                      style: TextStyle(color: Colors.black)),
+                  items: const [
+                    ListTile(
+                      mouseCursor: SystemMouseCursors.click,
+                      title: Text(
+                        'Sub 2',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    ),
+                    ListTile(
+                      mouseCursor: SystemMouseCursors.click,
+                      title: Text(
+                        'Sub 3',
+                        style: TextStyle(color: Colors.white),
+                      ),
+                    )
+                  ],
+                ),
+                const VerticalDivider(
+                  width: 2,
+                  thickness: 1,
+                  color: Colors.white,
+                ),
+              ],
+            ),
+          ),
+          const Expanded(
+              child: Center(
+            child: Text('Hover Menu'),
+          ))
+        ],
+      ),
+    );
+  }
+}
